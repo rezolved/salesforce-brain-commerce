fe0797dcc7038dfec3b6affa33ac19e40a68d451
Satyanarayana-rezolve <satyanarayanakondapally@rezolve.com>
2025-02-17 17:53:59 +0530

    feat(BC-Product-Ingestion-CHnages)

--- cartridges/int_brain_commerce/cartridge/scripts/constants.js
+++ cartridges/int_brain_commerce/cartridge/scripts/constants.js
@@ -5,8 +5,8 @@ var base = module.superModule;
 base = {
     PRODUCT_END_POINT: '/v1/product',
     FAQ_END_POINT: '/v1/faq',
-    BRAIN_COMMERCE_CONFIG_CUSTOM_OBJECT_ID: 'brainCommerce',
-    BRAIN_COMMERCE_CONFIG_CUSTOM_OBJECT_RECORD_ID: 'brainCommerce'
+    BRAIN_COMMERCE_CONFIG_CUSTOM_OBJECT_ID: 'brainCommerceConfigs',
+    BRAIN_COMMERCE_CONFIG_CUSTOM_OBJECT_RECORD_ID: 'brainCommerceConfigKey'
 };
 
 module.exports = base;
